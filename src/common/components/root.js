b6c3f3caa4300fc331d2b04b14f9685558a4abd4
Peter Kupietz <kuuup@users.noreply.github.com>
2016-04-26 17:00:12 +0200

    Update root.js

--- src/common/components/root.js
+++ src/common/components/root.js
@@ -1,16 +1,6 @@
 import React, { Component } from 'react';
 import { observer } from "mobx-react";
 import { inject } from 'react-tunnel';
-//import { connect } from 'mobx-connect';
-
-/*export default observer(({ appstate }) =>
-    <div>
-        <button onClick={ () => appstate.addItem('uuuu1234') }>zZzZzZ</button>
-        <ul>
-            { appstate.items.map((item, key) => <li key={ key }>{ item }</li>) }
-        </ul>
-    </div>
-);*/
 
 @observer
 class Root extends Component {
