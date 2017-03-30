5b3ab5f8149a2a0ecad8d28ce82a59a8ea03cb3b
Peter Kupietz <p.kupietz@socoto.com>
2017-03-30 17:06:43 +0200

    simplified router component

--- src/common/router.js
+++ src/common/router.js
@@ -1,5 +1,5 @@
 /* @flow */
-import React, { Component } from 'react';
+import React from 'react';
 import Root from './components/root';
 
 type Props = {
@@ -9,19 +9,6 @@ type Props = {
 export const routes: Map<string, React$Element<*>> = new Map();
 routes.set('/', <Root />);
 
-export default class Router extends Component {
-
-    props: Props;
-
-    constructor(props: Props) {
-        super(props);
-    }
-
-    render() {
-        if(this.props.path && routes.has(this.props.path))
-            return routes.get(this.props.path)
-
-        return null;
-    }
-
+export default function router(props: Props) {
+    return props.path && routes.has(props.path) ? routes.get(props.path) : null;
 }
