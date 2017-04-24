5e444e2aa908b73f1d21857dbea8f8d394887394
Peter Kupietz <kuuup@users.noreply.github.com>
2017-04-24 12:43:31 +0200

    Added socket.io client code

--- src/common/stores/appstate.js
+++ src/common/stores/appstate.js
@@ -1,16 +1,29 @@
 /* @flow */
-import { observable, action } from 'mobx';
+import { observable, action, autorun } from 'mobx';
+import io from 'socket.io-client';
 
 export default class AppState<Item> {
 
     @observable items: Array<Item> = [];
 
+    socket = null;
+
     constructor(initialState?: { appstate: AppState<Item> }) {
         this.items = initialState && initialState.appstate && initialState.appstate.items ? initialState.appstate.items : [];
+        if(typeof window === 'object') {
+            this.socket = io.connect('http://localhost:3000/')
+            this.socket.on('update-from-server', data => this.addRemoteItem(data));
+        }
     }
 
     @action
     addItem(item: Item) {
+        this.items.push(item);
+        if(this.socket) this.socket.emit('update-from-client', item);
+    }
+
+    @action
+    addRemoteItem(item: Item) {
         this.items.push(item);
     }
 
